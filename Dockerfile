7fa9d4ac23181eb7b43b11f309328658ddba48cf
kshitijwohlig <kshitij.gaikwad@wohlig.com>
2024-01-23 15:14:59 +0530

    Dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:16
+WORKDIR /app
+COPY package*.json ./
+COPY . .
+RUN npm install
+EXPOSE 3200
+CMD ["node","server.js"]
--- server.js
+++ server.js
@@ -3,15 +3,23 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 3200;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+app.get('/devops', (req, res) => {
+  res.send('Hello devops');
+});
+app.get('/about', (req, res) => {
+  res.send('Hello About');
+});
+app.get('/contact', (req, res) => {
+  res.send('Hello Contact');
+});
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
 });
